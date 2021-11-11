6aed1a294a0f4d9e338fc1389ff0f89950c0be16
Marc Denojean-Mairet <marc.denojeanmairet@hotmail.com>
2021-11-10 22:00:03 -0700

    more updates

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -2,3 +2,10 @@
 # Date: 2021-11-10
 # This script reads learning2014 data file for exercise 2.
 
+lrn14 <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t",header=TRUE)
+
+dim(lrn14)
+
+str(lrn14)
+
+
