09298d73497a95ffb46ecc928d071708092af366
Marc Denojean-Mairet <marc.denojeanmairet@hotmail.com>
2021-11-21 11:50:33 -0700

    updates

--- data/create_alc.R
+++ data/create_alc.R
@@ -11,17 +11,19 @@ math <- read.table ("~/IODS-project/IODS-project/data/student-mat.csv", sep = ";
 dim(por)
 dim(math)
 
+
 # Structure
 str(por)
 str(math)
 
-# Define own id for both datasets
-library(dplyr)
-por_id <- por %>% mutate(id=1000+row_number()) 
-math_id <- math %>% mutate(id=2000+row_number())
-
 # The below code is from: https://raw.githubusercontent.com/rsund/IODS-project/master/data/create_alc.R
 # Credits to: Reijo Sund
+
+library(dplyr); library(ggplot2)
+
+# Define own id for both datasets
+por_id <- por %>% mutate(id=1000+row_number()) 
+math_id <- math %>% mutate(id=2000+row_number())
 
 # Which columns vary in datasets
 free_cols <- c("id","failures","paid","absences","G1","G2","G3")
@@ -58,17 +60,66 @@ pormath <- por_id %>%
   # Join original free fields, because rounded means or first values may not be relevant
   inner_join(pormath_free,by=c("id.p"="id"),suffix=c("",".p")) %>%
   inner_join(pormath_free,by=c("id.m"="id"),suffix=c("",".m")) %>%
-  # Calculate other required variables  
-  ungroup %>% mutate(
-    alc_use = (Dalc + Walc) / 2,
-    high_use = alc_use > 2,
-    cid=3000+row_number()
-  )
 
-# My code resume.
+# Below code is from DataCamp.
 
-# Checking dimensions of new data set
-dim(pormath)
+
+# print out the column names of pormath
+colnames(pormath)
+
+# create a new data frame with only the joined columns
+alc <- select(pormath, one_of(join_cols))
+
+# columns that were not used for joining the data
+notjoined_columns <- colnames(math)[!colnames(math) %in% join_cols]
+
+# print out the columns not used for joining
+notjoined_columns
+
+# for every column name not used for joining...
+for(column_name in notjoined_columns) {
+  # select two columns from 'math_por' with the same original name
+  two_columns <- select(pormath, starts_with(column_name))
+  # select the first column vector of those two columns
+  first_column <- select(two_columns, 1)[[1]]
+  
+  # if that first column  vector is numeric...
+  if(is.numeric(first_column)) {
+    # take a rounded average of each row of the two columns and
+    # add the resulting vector to the alc data frame
+    alc[column_name] <- round(rowMeans(two_columns))
+  } else { # else if it's not numeric...
+    # add the first column vector to the alc data frame
+    alc[column_name] <- first_column
+  }
+}
+# define a new column alc_use by combining weekday and weekend alcohol use
+alc <- mutate(alc, alc_use = (Dalc + Walc) / 2)
+
+# define a new logical column 'high_use'
+alc <- mutate(alc, high_use = alc_use > 2)
+
+alc["alc_use"]
+# glimpse at the new combined data
+glimpse(alc)
+colnames(alc)
+dim(alc)
+
+g1 <- ggplot(data = alc, aes(x = alc_use, fill = sex))
+
+# define the plot as a bar plot and draw it
+g1 + geom_bar()
+
+# define a new logical column 'high_use'
+alc <- mutate(alc, high_use = alc_use > 2)
+
+# initialize a plot of 'high_use'
+g2 <- ggplot(alc, aes(high_use))
+
+# draw a bar plot of high_use by sex
+g2 + facet_wrap("sex") + geom_bar()
+
+glimpse(alc)
 
 # Save new data set to data folder
-write.csv(pormath,file="~/IODS-project/IODS-project/data/pormath.csv")
+write.csv(alc,file="~/IODS-project/IODS-project/data/alc.csv")
