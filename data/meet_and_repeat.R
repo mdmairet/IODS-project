a2db853b570cd04a9b609604bb69dd45a3f099bc
Marc Denojean-Mairet <marc.denojeanmairet@hotmail.com>
2021-12-07 15:51:09 -0700

    updates

--- data/meet_and_repeat.R
+++ data/meet_and_repeat.R
@@ -1,6 +1,13 @@
 # Marc Denojean-Mairet
 # Chapter 6: Analysis of longitudinal data
 
+# libraries
+library(dplyr)
+library(tidyr)
+
+# part 1: Load the data sets (BPRS and RATS) into R using as the source the GitHub repository of MABS
+
+# load BPRS data
 BPRS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", sep = " ", header = TRUE)
 
 # Look at the (column) names of BPRS
@@ -12,6 +19,9 @@ str(BPRS)
 # print out summaries of the variables
 summary(BPRS)
 
+
+
+# load rats data
 RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", sep = "\t", header = TRUE)
 
 # Look at the (column) names of BPRS
@@ -22,3 +32,20 @@ str(RATS)
 
 # print out summaries of the variables
 summary(RATS)
+
+# part 2: Convert the categorical variables of both data sets to factors
+
+
+# Factor treatment & subject
+BPRS$treatment <- factor(BPRS$treatment)
+BPRS$subject <- factor(BPRS$subject)
+
+# Take a glimpse at the BPRSL data
+glimpse(BPRSL)
+
+# Factor variables ID and Group
+RATS$ID <- factor(RATS$ID)
+RATS$Group <- factor(RATS$Group)
+
+# Glimpse the data
+glimpse(RATS)
