3e526e0e42ad173c0c998649939b4b09f1c00d39
Marc Denojean-Mairet <marc.denojeanmairet@hotmail.com>
2021-12-07 14:26:27 -0700

    updates

--- data/meet_and_repeat.R
+++ data/meet_and_repeat.R
@@ -1,3 +1,24 @@
 # Marc Denojean-Mairet
 # Chapter 6: Analysis of longitudinal data
 
+BPRS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", sep = " ", header = TRUE)
+
+# Look at the (column) names of BPRS
+names(BPRS)
+
+# Look at the structure of BPRS
+str(BPRS)
+
+# print out summaries of the variables
+summary(BPRS)
+
+RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", sep = "\t", header = TRUE)
+
+# Look at the (column) names of BPRS
+names(RATS)
+
+# Look at the structure of BPRS
+str(RATS)
+
+# print out summaries of the variables
+summary(RATS)
